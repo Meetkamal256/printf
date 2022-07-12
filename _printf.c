609b32bd7ee38f5ab5017d774067d4edf3fa79e4
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2022-07-12 09:46:20 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,38 +1,51 @@
 #include "main.h"
 
+/**
+ * _printf - a function that produces output according to
+ * a format.
+ * @format: The format to be used in printing
+ * @...: Arguments list
+ *
+ * Return: (int) the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, x;
-    char *str;
+	va_list args, args_copy;
+	int i = 0, j, count, spec_count = 0;
+	char c, next_c, *spec = NULL;
+	int (*op)(int, char *, va_list) = NULL;
 
-    va_start(args, format);
-
-    while (*format)
-    {
-
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-            case 'c':
-                _putchar(va_arg(args, int));
-                format++;
-                continue;
-            case 's':
-                str = va_arg(args, char *);
-                if (!str)
-                    str = "(nil)";
-                for (; *str; str++)
-                    _putchar(*str);
-                format++;
-                continue;
-            default:
-                continue;
-                ;
-            }
-        }
-        _putchar(*format++);
-    }
+	va_start(args, format);
+	count = (format != NULL) ? 0 : -1;
+	while (format != NULL && format[i] != '\0')
+	{
+		c = format[i];
+		next_c = format[i + 1];
+		if (c == '%' && _is_specifier(next_c) == 1)
+		{
+			spec = _realloc(NULL, 0, 2);
+			j = 0;
+			while (format[++i] != '\0' && op == NULL && spec != NULL)
+			{
+				spec[j++] = format[i];
+				spec[j] = '\0';
+				op = get_print_function(spec);
+				if (op != NULL)
+				{
+					va_copy(args_copy, args);
+					count += op(spec_count++, spec, args_copy);
+					free(spec);
+				}
+				spec = _realloc(spec, _strlen(spec), _strlen(spec) + 2);
+			}
+			op = NULL;
+			continue;
+		}
+		i = (c == '%') ? i + 1 : i;
+		c = (c == '%') ? next_c : c;
+		count += _putchar(c);
+		++i;
+	}
+	va_end(args);
+	return (count);
 }
